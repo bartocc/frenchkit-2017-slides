0b017452a60368787267a35f989e823d3d6bb3fa
Julien Palmas <julien.palmas@gmail.com>
2017-09-21 00:45:27 +0200

    add ember-cli slides

---
+++ code-samples/cli.sh
@@ -0,0 +1,24 @@
+# sample(model)
+$ ember generate model product
+
+installing model
+  create app/product/model.js
+installing model-test
+  create tests/unit/product/model-test.js
+# end-sample
+
+# sample(route)
+$ ember generate route products
+
+installing route
+  create app/products/route.js
+  create app/products/template.hbs
+updating router
+  add route products
+installing route-test
+  create tests/unit/products/route-test.js
+# end-sample
+
+# sample(install-mirage)
+$ ember install ember-cli-mirage
+# end-sample
