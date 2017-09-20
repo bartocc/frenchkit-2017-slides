c5c40160611fb35e989ac3fc7ce1dabe6330f586
Julien Palmas <julien.palmas@gmail.com>
2017-09-20 16:14:51 +0200

    more content

--- code-samples/mirage.js
+++ code-samples/mirage.js
@@ -1,7 +1,15 @@
-// sample(s1)
+// sample(post)
 // app/mirage/config.js
 export default function() {
+  // this route will handle a POST request
+  // to the URL '/products'
+  this.post('/products');
+};
+// end-sample
 
+// sample(namespace)
+// app/mirage/config.js
+export default function() {
   this.namespace = '/api';
 
   // this route will handle a POST request
@@ -10,6 +18,22 @@ export default function() {
 };
 // end-sample
 
-// sample(s2)
-var foo = "bar"
+// sample(timing)
+// app/mirage/config.js
+export default function() {
+
+  // Simulate a slow response from the server
+  this.post('/users', { timing: 1500 });
+};
+// end-sample
+
+// sample(rest)
+// app/mirage/config.js
+export default function() {
+
+  // Define different
+  this.get('/api/authors');
+  this.put('/api/authors/:id');
+  this.del('/posts/:id');
+};
 // end-sample
