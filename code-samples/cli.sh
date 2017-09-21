3e7042d548b9e22ec38298961c7f8fbab83a5b1e
Julien Palmas <julien.palmas@gmail.com>
2017-09-21 09:55:30 +0200

    add tree

--- code-samples/cli.sh
+++ code-samples/cli.sh
@@ -22,3 +22,31 @@ installing route-test
 # sample(install-mirage)
 $ ember install ember-cli-mirage
 # end-sample
+
+# sample(new-project)
+$ ember new my-awesome-ember-app
+# end-sample
+
+# sample(tree)
+$ tree -L 2 .
+├── README.md
+├── app
+│   ├── app.js
+│   ├── components
+│   ├── controllers
+│   ├── helpers
+│   ├── index.html
+│   ├── models
+│   ├── resolver.js
+│   ├── router.js
+│   ├── routes
+│   ├── styles
+│   └── templates
+├── config
+├── ember-cli-build.js
+├── package.json
+├── public
+├── testem.js
+├── tests
+└── vendor
+# end-sample
